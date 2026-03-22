2bddf9055a34afa7f8f7b4b7538cbf0b9d58843b
zubair ashiq <zashly.automation@gmail.com>
2026-03-22 03:07:00 -0700

    vite-changes-vercel

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VERCEL ? '/' : '/zash-lab-001/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),

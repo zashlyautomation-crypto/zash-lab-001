5d7266b2c4ed1ba512130f95ba67c9728bc4ca02
zubair ashiq <zashly.automation@gmail.com>
2026-03-22 02:28:33 -0700

    vites-changes

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/zash-lab-001/',
   plugins: [
     react(),
     tailwindcss(),
   ],
+
 })

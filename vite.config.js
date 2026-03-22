c227ed33e54aea5ceff6cc42b3a7b6e47df8a07d
zubair ashiq <zashly.automation@gmail.com>
2026-03-22 03:04:27 -0700

    vite-changes-after-deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/zash-lab-001/',
+  base: process.env.VERCEL ? '/' : '/zash-lab-001/',
   plugins: [
     react(),
     tailwindcss(),
